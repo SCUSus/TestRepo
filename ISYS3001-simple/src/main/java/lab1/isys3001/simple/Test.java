c82defa1cebc7546e7c573f8ddd499d9e5382597
SCUSus <susanne.brun@scu.edu.au>
2021-07-19 22:27:18 +1000

    First commit from NetBeans

---
+++ ISYS3001-simple/src/main/java/lab1/isys3001/simple/Test.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1.isys3001.simple;
+
+/**
+ * Empty class for testing
+ * @author Susanne
+ */
+public class Test {
+    
+}
